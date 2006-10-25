a07e5a307b98dfc91387b723cc040754ea2375d4
Daniel P. Berrange <berrange@redhat.com>
2006-10-25 09:38:17 -0400

    Disable wizard sensitivity while VM creation is taking place (bz 211624)

--- src/virtManager/create.py
+++ src/virtManager/create.py
@@ -371,9 +371,11 @@ class vmmCreate(gobject.GObject):
 
         #let's go
         self.install_error = None
+        self.topwin.set_sensitive(False)
         progWin = vmmAsyncJob(self.config, self.do_install, [guest],
                               title=_("Creating Virtual Machine"))
         progWin.run()
+        self.topwin.set_sensitive(True)
         if self.install_error != None:
             logging.error("Async job failed to create VM " + str(self.install_error))
             self._validation_error_box(_("Guest Install Error"), self.install_error)
