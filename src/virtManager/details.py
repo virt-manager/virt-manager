9e46f8bdb5dd45f9b14775fdd25d8943c8c57f7e
Michal Novotny <minovotn@redhat.com>
2009-06-16 15:36:10 -0400

    Make Processor tab item in VM details show correctly
    The Processor tab in VM details was not showing at all because of exception.
    The exception occured everytime the tab was selected because of call to non
    existing function get_type() that has been renamed to get_hv_type() in the
    newest version of virt-manager's code by revision 1135.

--- src/virtManager/details.py
+++ src/virtManager/details.py
@@ -945,7 +945,7 @@ class vmmDetails(gobject.GObject):
         self.window.get_widget("state-host-cpus").set_text("%d" % self.vm.get_connection().host_active_processor_count())
         status = self.vm.status()
         if status in [ libvirt.VIR_DOMAIN_SHUTOFF, libvirt.VIR_DOMAIN_CRASHED ]:
-            cpu_max = self.vm.get_connection().get_max_vcpus(self.vm.get_type())
+            cpu_max = self.vm.get_connection().get_max_vcpus(self.vm.get_hv_type())
             self.window.get_widget("config-vcpus").get_adjustment().upper = cpu_max
             self.window.get_widget("state-vm-maxvcpus").set_text(str(cpu_max))
         else:
