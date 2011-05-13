fdd07b26e48e0e4c7bfc402b4dbd096b0fc754a6
Darryl L. Pierce <dpierce@redhat.com>
2011-05-13 10:56:57 -0400

    Updated ListDomainsConfigScreen to use vmmDomain types.

--- src/virtManagerTui/listdomains.py
+++ src/virtManagerTui/listdomains.py
@@ -49,18 +49,18 @@ class ListDomainsConfigScreen(DomainListConfigScreen):
             return self.get_detail_page_elements(screen)
 
     def get_detail_page_elements(self, screen):
-        domain = self.get_libvirt().get_domain(self.get_selected_domain())
+        domain = self.get_selected_domain()
         grid = Grid(2, 5)
         grid.setField(Label("Name:  "), 0, 0, anchorRight = 1)
-        grid.setField(Label(domain.name()), 1, 0, anchorLeft = 1)
+        grid.setField(Label(domain.get_name()), 1, 0, anchorLeft = 1)
         grid.setField(Label("UUID:  "), 0, 1, anchorRight = 1)
-        grid.setField(Label(domain.UUIDString()), 1, 1, anchorLeft = 1)
+        grid.setField(Label(domain.get_uuid()), 1, 1, anchorLeft = 1)
         grid.setField(Label("OS Type:  "), 0, 2, anchorRight = 1)
-        grid.setField(Label(domain.OSType()), 1, 2, anchorLeft = 1)
+        grid.setField(Label(domain.get_abi_type()), 1, 2, anchorLeft = 1)
         grid.setField(Label("Max. Memory:  "), 0, 3, anchorRight = 1)
-        grid.setField(Label(str(domain.maxMemory())), 1, 3, anchorLeft = 1)
+        grid.setField(Label(str(domain.maximum_memory())), 1, 3, anchorLeft = 1)
         grid.setField(Label("Max. VCPUs:  "), 0, 4, anchorRight = 1)
-        grid.setField(Label(str(domain.maxVcpus())), 1, 4, anchorLeft = 1)
+        grid.setField(Label(str(domain.vcpu_count())), 1, 4, anchorLeft = 1)
         return [grid]
 
 def ListDomains():
