dc640d7506d95c3b3fc601418af138003a516366
Cole Robinson <crobinso@redhat.com>
2012-11-07 13:15:26 +0100

    configure: Don't drop so many files in topdir

--- autogen.sh
+++ autogen.sh
@@ -3,10 +3,15 @@
 set -e
 set -x
 
+# Remove config.status, so rerunning autogen.sh regenerates everything
 rm -f config.status
 
-# Make makefiles.
+# Touch this to work around gettext issues following intltool I18N-HOWTO:
+# http://lists.gnu.org/archive/html/bug-gettext/2011-10/msg00012.html
+mkdir -p build-aux/m4
+touch build-aux/config.rpath
+
 autoreconf --force --install --verbose
 intltoolize --force --copy --automake
 
-./configure $@
+./configure "$@"
