1abd03ed80b47a5506798d341924e2b3d577a8fc
Cole Robinson <crobinso@redhat.com>
2012-12-20 12:51:45 -0500

    Fix packagekit package install on KDE with apper
    Apper requires a uint, gnome-packagekit doesn't seem to care either way
    https://bugzilla.redhat.com/show_bug.cgi?id=882024

--- src/virtManager/packageutils.py
+++ src/virtManager/packageutils.py
@@ -131,7 +131,8 @@ def packagekit_install(package_list):
     # Set 2 hour timeout
     timeout = 60 * 60 * 2
     logging.debug("Installing packages: %s", package_list)
-    pk_control.InstallPackageNames(0, package_list, "hide-confirm-search",
+    pk_control.InstallPackageNames(dbus.Uint32(0),
+                                   package_list, "hide-confirm-search",
                                    timeout=timeout)
 
 def packagekit_search(session, pk_control, package_name, packages):
