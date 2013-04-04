3f7b46884ebbd5da83435f7408448e5bf77ad549
Cole Robinson <crobinso@redhat.com>
2013-04-03 20:28:49 -0400

    Fix icons when running from srcdir

--- virtcli/cliconfig.py
+++ virtcli/cliconfig.py
@@ -32,7 +32,6 @@ cfgpath = os.path.join(os.path.dirname(_filepath), "cli.cfg")
 if os.path.exists(cfgpath):
     cfg.read(cfgpath)
 
-
 def _split_list(commastr):
     return [d for d in commastr.split(",") if d]
 
@@ -46,15 +45,12 @@ __version__ = "0.9.4"
 
 # We should map this into the config somehow but I question if anyone cares
 prefix = "/usr"
+gettext_dir = os.path.join(prefix, "share", "locale")
 if os.getcwd() == _srcdir:
     asset_dir = _srcdir
-    # XXX: This gettext bit likely doesn't work either, maybe nothing we can do
-    gettext_dir = os.path.join(_srcdir, "po")
-    # XXX: This wants data/icons/hicolor...
-    icon_dir = os.path.join(_srcdir, "data", "icons")
+    icon_dir = os.path.join(_srcdir, "data")
 else:
     asset_dir = os.path.join(prefix, "share", "virt-manager")
-    gettext_dir = os.path.join(prefix, "share", "locale")
     icon_dir = os.path.join(asset_dir, "icons")
 
 with_tui = bool(int(get_param("with_tui", "1")))
