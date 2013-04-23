c6d5e5d2674dfa55b1bd8a3df9f5584b43c6a4b7
Cole Robinson <crobinso@redhat.com>
2013-04-23 12:27:49 -0400

    config: Fix crash when launching create wizard
    Caused by accessing invalid gsettings key

--- virtManager/config.py
+++ virtManager/config.py
@@ -460,16 +460,16 @@ class vmmConfig(object):
             self.conf.set(gconf_path, urls)
 
     def add_media_url(self, url):
-        self._url_add_helper("/urls/media", url)
+        self._url_add_helper("/urls/urls", url)
     def add_kickstart_url(self, url):
-        self._url_add_helper("/urls/kickstart", url)
+        self._url_add_helper("/urls/kickstarts", url)
     def add_iso_path(self, path):
         self._url_add_helper("/urls/isos", path)
 
     def get_media_urls(self):
-        return self.conf.get("/urls/media")
+        return self.conf.get("/urls/urls")
     def get_kickstart_urls(self):
-        return self.conf.get("/urls/kickstart")
+        return self.conf.get("/urls/kickstarts")
     def get_iso_paths(self):
         return self.conf.get("/urls/isos")
 
