edf9bf0aad2319c3df58d36fb89266fa4c6e8429
Cole Robinson <crobinso@redhat.com>
2014-09-19 20:35:06 -0400

    serialcon: Drop use of yet more removed API

--- virtManager/serialcon.py
+++ virtManager/serialcon.py
@@ -326,7 +326,6 @@ class vmmSerialConsole(vmmGObject):
         self.terminal = Vte.Terminal()
         self.terminal.set_scrollback_lines(1000)
         self.terminal.set_audible_bell(False)
-        self.terminal.set_visible_bell(True)
 
         self.terminal.connect("button-press-event", self.show_serial_rcpopup)
         self.terminal.connect("commit", self.console.send_data, self.terminal)
