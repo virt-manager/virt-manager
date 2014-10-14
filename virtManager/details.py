virt-manager/virt-manager

commit 5e8f35f5e6d2f86a6846e3725885980eb51a5eae

virt-manager, details: fix path and size for disk type='volume'
Closes: https://bugzilla.redhat.com/show_bug.cgi?id=1146869

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

virtManager/details.py
@@ -2559,6 +2559,7 @@ class vmmDetails(vmmGObjectUI):
             return
 
         path = disk.path
+        source_pool = disk.sourcePool
         devtype = disk.device
         ro = disk.read_only
         share = disk.shareable
@@ -2584,7 +2585,13 @@ class vmmDetails(vmmGObjectUI):
         if not path:
             size = "-"
         else:
-            vol = self.conn.get_vol_by_path(path)
+            if source_pool:
+                pool = self.conn.get_pool(source_pool)
+                if pool:
+                    vol = pool.get_volume(path)
+            else:
+                vol = self.conn.get_vol_by_path(path)
+
             if vol:
                 size = vol.get_pretty_capacity()
             elif not self.conn.is_remote():

commit 750eeaad988f3670c1d3f0ae0caf899040092ff4

virt-manager, details: properly check for pool presence
commit 5e8f35f5e6d2f86a6846e3725885980eb51a5eae introduced this
problem.

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

virtManager/details.py
@@ -2586,9 +2586,11 @@ class vmmDetails(vmmGObjectUI):
             size = "-"
         else:
             if source_pool:
-                pool = self.conn.get_pool(source_pool)
-                if pool:
+                try:
+                    pool = self.conn.get_pool(source_pool)
                     vol = pool.get_volume(path)
+                except KeyError:
+                    vol = None
             else:
                 vol = self.conn.get_vol_by_path(path)
 