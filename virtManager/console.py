0b391fe9eda81e6c01ba37171190a8d9b2a51aad
Cole Robinson <crobinso@redhat.com>
2014-12-02 14:01:28 -0500

    virt-manager: Enforce gobject introspection versions
    This is needed with freebsd and opensuse at least. See:
    
    https://bugzilla.suse.com/__show_bug.cgi?id=901869
    http://www.redhat.com/archives/libvir-list/2014-December/msg00184.html

--- virtManager/console.py
+++ virtManager/console.py
@@ -20,10 +20,13 @@
 # MA 02110-1301 USA.
 #
 
+import gi
 from gi.repository import GObject
 from gi.repository import Gtk
 from gi.repository import Gdk
+gi.require_version('GtkVnc', '2.0')
 from gi.repository import GtkVnc
+gi.require_version('SpiceClientGtk', '3.0')
 from gi.repository import SpiceClientGtk
 from gi.repository import SpiceClientGLib
 
