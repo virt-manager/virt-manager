8369c65966ef22bab4efe80bdc58cb47c53939e4
Cole Robinson <crobinso@redhat.com>
2015-04-24 21:01:55 -0400

    storage: Use ~/.config/libvirt/images for qemu:///session
    It doesn't create the annoying ~/VirtualMachines directory, and already
    has proper labelling.

--- virtinst/storage.py
+++ virtinst/storage.py
@@ -82,7 +82,7 @@ class _StorageObject(XMLBuilder):
 def _get_default_pool_path(conn):
     path = "/var/lib/libvirt/images"
     if conn.is_session_uri():
-        path = os.path.expanduser("~/VirtualMachines")
+        path = os.path.expanduser("~/.config/libvirt/images")
     return path
 
 
