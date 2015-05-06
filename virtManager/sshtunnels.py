virt-manager/virt-manager

commit a2d453f3e20d103a4767394300c5183fde9a6bb4

sshtunnels: use ipaddr to check if address is loopback or unspecified
It supports IPv6 addresses written as "0::0", "0:0::0".

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

virtManager/sshtunnels.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2014 Red Hat, Inc.
+# Copyright (C) 2014, 2015 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,6 +23,7 @@ import Queue
 import socket
 import signal
 import threading
+import ipaddr
 
 from .baseclass import vmmGObject
 
@@ -47,10 +48,10 @@ class ConnectionInfo(object):
             self._connhost = "127.0.0.1"
 
     def _is_listen_localhost(self, host=None):
-        return (host or self.gaddr) in ["127.0.0.1", "::1"]
+        return ipaddr.IPNetwork(host or self.gaddr).is_loopback
 
     def _is_listen_any(self):
-        return self.gaddr in ["0.0.0.0", "::"]
+        return ipaddr.IPNetwork(self.gaddr).is_unspecified
 
     def need_tunnel(self):
         if not self._is_listen_localhost():

commit ebcb7c064ca5a3afd2ec3a0c8f59328a7f71b009

sshtunnels: fix exception when the address is not an IP
bug introduced with commit a2d453f3e20d103a4767394300c5183fde9a6bb4

Closes: https://bugzilla.redhat.com/show_bug.cgi?id=1218958

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

virtManager/sshtunnels.py
@@ -48,10 +48,16 @@ class ConnectionInfo(object):
             self._connhost = "127.0.0.1"
 
     def _is_listen_localhost(self, host=None):
-        return ipaddr.IPNetwork(host or self.gaddr).is_loopback
+        try:
+            return ipaddr.IPNetwork(host or self.gaddr).is_loopback
+        except:
+            return False
 
     def _is_listen_any(self):
-        return ipaddr.IPNetwork(self.gaddr).is_unspecified
+        try:
+            return ipaddr.IPNetwork(self.gaddr).is_unspecified
+        except:
+            return False
 
     def need_tunnel(self):
         if not self._is_listen_localhost():