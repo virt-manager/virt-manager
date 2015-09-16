5df6757a0bd9b8f4dcca17a73bad3d9fe46d7823
Cole Robinson <crobinso@redhat.com>
2015-09-16 16:46:39 -0400

    viewers: Use GtkVnc.Display() not GtkVnc.Display.new()
    The latter doesn't cause problems on Fedora, but it's currently throwing
    errors on Freebsd:
    
    https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=203152

--- virtManager/viewers.py
+++ virtManager/viewers.py
@@ -272,7 +272,7 @@ class VNCViewer(Viewer):
     ###################
 
     def _init_widget(self):
-        self._display = GtkVnc.Display.new()
+        self._display = GtkVnc.Display()
 
         # Make sure viewer doesn't force resize itself
         self._display.set_force_size(False)
