450be4a259c9b549a71fd4e1004b6895e6600c2c
Cole Robinson <crobinso@redhat.com>
2015-12-24 13:18:32 -0500

    Prep for release 1.3.2

--- virtcli/cliconfig.py
+++ virtcli/cliconfig.py
@@ -68,7 +68,7 @@ def _setup_gsettings_path(schemadir):
         raise RuntimeError("Failed to compile local gsettings schemas")
 
 
-__version__ = "1.3.1"
+__version__ = "1.3.2"
 
 
 class _CLIConfig(object):
