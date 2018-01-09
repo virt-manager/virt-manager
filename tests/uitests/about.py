7a12899b44d8a3342cfc77fc87aa69d2eb766b7c
Cole Robinson <crobinso@redhat.com>
2018-01-09 14:48:42 -0500

    uitests: Add test for 'about' dialog

---
+++ tests/uitests/about.py
@@ -0,0 +1,26 @@
+import datetime
+
+from tests.uitests import utils as uiutils
+
+
+class VMMCLI(uiutils.UITestCase):
+    """
+    UI tests for virt-manager's command line --show options
+    """
+
+    ##############
+    # Test cases #
+    ##############
+
+    def testAbout(self):
+        uiutils.find_pattern(self.app.root, "Help", "menu").click()
+        uiutils.find_pattern(self.app.root, "About", "menu item").click()
+        win = uiutils.find_fuzzy(self.app.root, "About", "dialog")
+        l = uiutils.find_fuzzy(win, "Copyright", "label")
+
+        curyear = datetime.datetime.today().strftime("%Y")
+        if curyear not in l.text:
+            print("Current year=%s not in about.ui dialog!" % curyear)
+
+        win.keyCombo("<ESC>")
+        uiutils.check_in_loop(lambda: win.visible is False)
