virt-manager/virt-manager

commit dae8642bb1e094963068e6082c3d2ff7d33f3696

xmlbuilder: centralize adding child new child prop instances
Currently the domain CPU class has a child property like:

  siblings = XMLChildProperty(_CPUCellSibling)

If a user wants to add a new sibling, we add a convenience function:

    def add_sibling(self):
        obj = _CPUCellSibling(self.conn)
        self.add_child(obj)
        return obj

Rather than require every child property to define a similar matching
helper function, this adds infrastructure in xmlbuilder to do this
generically for every child property. Now callers can do

    obj = guest.cpu.siblings.add_new()

virtManager/createnet.py
@@ -756,7 +756,7 @@ class vmmCreateNetwork(vmmGObjectUI):
 
         if net.forward.mode == "hostdev":
             net.forward.managed = "yes"
-            pfobj = net.forward.add_pf()
+            pfobj = net.forward.pfs.add_new()
             pfobj.dev = net.forward.dev
             net.forward.dev = None
             net.domain_name = None
@@ -765,12 +765,12 @@ class vmmCreateNetwork(vmmGObjectUI):
 
         if self.get_config_ipv4_enable():
             ip = self.get_config_ip4()
-            ipobj = net.add_ip()
+            ipobj = net.ips.add_new()
             ipobj.address = str(ip.network_address + 1)
             ipobj.netmask = str(ip.netmask)
 
             if self.get_config_dhcpv4_enable():
-                dhcpobj = ipobj.add_range()
+                dhcpobj = ipobj.ranges.add_new()
                 dhcpobj.start = str(
                     self.get_config_dhcpv4_start().network_address
                 )
@@ -778,13 +778,13 @@ class vmmCreateNetwork(vmmGObjectUI):
 
         if self.get_config_ipv6_enable():
             ip = self.get_config_ip6()
-            ipobj = net.add_ip()
+            ipobj = net.ips.add_new()
             ipobj.family = "ipv6"
             ipobj.address = str(ip.network_address + 1)
             ipobj.prefix = str(ip.prefixlen)
 
             if self.get_config_dhcpv6_enable():
-                dhcpobj = ipobj.add_range()
+                dhcpobj = ipobj.ranges.add_new()
                 dhcpobj.start = str(
                     self.get_config_dhcpv6_start().network_address
                 )
@@ -795,7 +795,7 @@ class vmmCreateNetwork(vmmGObjectUI):
         netaddr = _make_ipaddr(self.get_config_routev4_network())
         gwaddr = _make_ipaddr(self.get_config_routev4_gateway())
         if netaddr and gwaddr:
-            route = net.add_route()
+            route = net.routes.add_new()
             route.family = "ipv4"
             route.address = netaddr.network_address
             route.prefix = netaddr.prefixlen
@@ -804,7 +804,7 @@ class vmmCreateNetwork(vmmGObjectUI):
         netaddr = _make_ipaddr(self.get_config_routev6_network())
         gwaddr = _make_ipaddr(self.get_config_routev6_gateway())
         if netaddr and gwaddr:
-            route = net.add_route()
+            route = net.routes.add_new()
             route.family = "ipv6"
             route.address = netaddr.network_address
             route.prefix = netaddr.prefixlen

tests/storage.py
@@ -40,7 +40,8 @@ def createPool(conn, ptype, poolname=None, fmt=None, target_path=None,
     pool_inst.type = ptype
 
     if pool_inst.supports_property("hosts"):
-        pool_inst.add_host("some.random.hostname")
+        hostobj = pool_inst.hosts.add_new()
+        hostobj.name = "some.random.hostname"
     if pool_inst.supports_property("source_path"):
         pool_inst.source_path = source_path or "/some/source/path"
     if pool_inst.supports_property("target_path"):

tests/xmlparse.py
@@ -120,11 +120,11 @@ class XMLParseTest(unittest.TestCase):
 
         check = self._make_checker(guest.clock)
         check("offset", "utc", "localtime")
-        guest.clock.remove_timer(guest.clock.timers[0])
+        guest.clock.remove_child(guest.clock.timers[0])
         check = self._make_checker(guest.clock.timers[0])
         check("name", "pit", "rtc")
         check("tickpolicy", "delay", "merge")
-        timer = guest.clock.add_timer()
+        timer = guest.clock.timers.add_new()
         check = self._make_checker(timer)
         check("name", None, "hpet")
         check("present", None, False)
@@ -180,7 +180,7 @@ class XMLParseTest(unittest.TestCase):
         check("name", "x2apic")
         check("policy", "force", "disable")
         rmfeat = guest.cpu.features[3]
-        guest.cpu.remove_feature(rmfeat)
+        guest.cpu.remove_child(rmfeat)
         self.assertEqual(rmfeat.get_xml_config(),
                           """<feature name="foo" policy="bar"/>\n""")
         guest.cpu.add_feature("addfeature")
@@ -1196,7 +1196,9 @@ class XMLParseTest(unittest.TestCase):
         check = self._make_checker(pool.hosts[2])
         check("name", "ceph-mon-3.example.com")
         check("port", 6789, 1000)
-        pool.add_host("frobber", "5555")
+        hostobj = pool.hosts.add_new()
+        hostobj.name = "frobber"
+        hostobj.port = "5555"
 
         utils.diff_compare(pool.get_xml_config(), outfile)
         utils.test_create(conn, pool.get_xml_config(), "storagePoolDefineXML")
@@ -1287,7 +1289,7 @@ class XMLParseTest(unittest.TestCase):
         check("family", "ipv6", "ipv6")
         check("prefix", 64, 63)
 
-        r = net.add_route()
+        r = net.routes.add_new()
         r.family = "ipv4"
         r.address = "192.168.8.0"
         r.prefix = "24"

virtManager/createpool.py
@@ -514,7 +514,8 @@ class vmmCreatePool(vmmGObjectUI):
         try:
             self._pool.target_path = target
             if host:
-                self._pool.add_host(host)
+                hostobj = self._pool.hosts.add_new()
+                hostobj.name = host
             if source:
                 self._pool.source_path = source
             if fmt:

virtinst/cli.py
@@ -1143,7 +1143,7 @@ class VirtCLIParser(object):
         # end of the domain XML, which gives an ugly diff
         clear_inst.clear(leave_stub="," in self.optstr)
 
-    def _make_find_inst_cb(self, cliarg, objpropname, objaddfn):
+    def _make_find_inst_cb(self, cliarg, objpropname):
         """
         Create a callback used for find_inst_cb command line lookup.
 
@@ -1152,9 +1152,6 @@ class VirtCLIParser(object):
         :param objpropname: The property name on the virtinst object that
             this parameter maps too. For the seclabel example, we want
             disk.seclabels, so this value is 'seclabels'
-        :param objaddfn: The function name for adding a new instance of
-            this parameter to the virtinst object. For the seclabel example,
-            we want disk.add_seclabel(), so this value is "add_seclabels"
         """
         def cb(inst, val, virtarg, can_edit):
             ignore = val
@@ -1165,7 +1162,7 @@ class VirtCLIParser(object):
 
             if can_edit:
                 while len(getattr(inst, objpropname)) < (num + 1):
-                    getattr(inst, objaddfn)()
+                    getattr(inst, objpropname).add_new()
             try:
                 return getattr(inst, objpropname)[num]
             except IndexError:
@@ -1465,8 +1462,7 @@ class ParserCPU(VirtCLIParser):
     def cell_find_inst_cb(self, *args, **kwargs):
         cliarg = "cell"  # cell[0-9]*
         objpropname = "cells"  # cpu.cells
-        objaddfn = "add_cell"  # cpu.add_cell
-        cb = self._make_find_inst_cb(cliarg, objpropname, objaddfn)
+        cb = self._make_find_inst_cb(cliarg, objpropname)
         return cb(*args, **kwargs)
 
     def sibling_find_inst_cb(self, inst, *args, **kwargs):
@@ -1475,8 +1471,7 @@ class ParserCPU(VirtCLIParser):
 
         cliarg = "sibling"  # cell[0-9]*.distances.sibling[0-9]*
         objpropname = "siblings"  # cell.siblings
-        objaddfn = "add_sibling"  # cell.add_sibling
-        cb = self._make_find_inst_cb(cliarg, objpropname, objaddfn)
+        cb = self._make_find_inst_cb(cliarg, objpropname)
         return cb(inst, *args, **kwargs)
 
     def set_model_cb(self, inst, val, virtarg):
@@ -1508,8 +1503,8 @@ class ParserCPU(VirtCLIParser):
     def set_l3_cache_cb(self, inst, val, virtarg, can_edit):
         cpu = inst
 
-        if can_edit:
-            cpu.set_l3_cache_mode()
+        if can_edit and not cpu.cache:
+            cpu.cache.add_new()
         try:
             return cpu.cache[0]
         except IndexError:
@@ -1580,8 +1575,7 @@ class ParserCPUTune(VirtCLIParser):
     def vcpu_find_inst_cb(self, *args, **kwargs):
         cliarg = "vcpupin"  # vcpupin[0-9]*
         objpropname = "vcpus"
-        objaddfn = "add_vcpu"
-        cb = self._make_find_inst_cb(cliarg, objpropname, objaddfn)
+        cb = self._make_find_inst_cb(cliarg, objpropname)
         return cb(*args, **kwargs)
 
 _register_virt_parser(ParserCPUTune)
@@ -1823,7 +1817,7 @@ class ParserClock(VirtCLIParser):
                 break
 
         if not timerobj:
-            timerobj = inst.add_timer()
+            timerobj = inst.timers.add_new()
             timerobj.name = tname
 
         setattr(timerobj, attrname, val)
@@ -1928,11 +1922,14 @@ class ParserQemuCLI(VirtCLIParser):
 
     def args_cb(self, inst, val, virtarg):
         for opt in shlex.split(val):
-            inst.add_arg(opt)
+            obj = inst.args.add_new()
+            obj.value = opt
 
     def env_cb(self, inst, val, virtarg):
         name, envval = val.split("=", 1)
-        inst.add_env(name, envval)
+        obj = inst.envs.add_new()
+        obj.name = name
+        obj.value = envval
 
     def _parse(self, inst):
         self.optdict.clear()
@@ -2023,8 +2020,7 @@ class ParserDisk(VirtCLIParser):
     def seclabel_find_inst_cb(self, *args, **kwargs):
         cliarg = "seclabel"  # seclabel[0-9]*
         objpropname = "seclabels"  # disk.seclabels
-        objaddfn = "add_seclabel"  # disk.add_seclabel
-        cb = self._make_find_inst_cb(cliarg, objpropname, objaddfn)
+        cb = self._make_find_inst_cb(cliarg, objpropname)
         return cb(*args, **kwargs)
 
     def _parse(self, inst):
@@ -2303,7 +2299,7 @@ class ParserGraphics(VirtCLIParser):
             inst.set_listen_none()
         elif val == "socket":
             inst.remove_all_listens()
-            obj = inst.add_listen()
+            obj = inst.listens.add_new()
             obj.type = "socket"
         else:
             inst.listen = val
@@ -2311,8 +2307,7 @@ class ParserGraphics(VirtCLIParser):
     def listens_find_inst_cb(self, *args, **kwargs):
         cliarg = "listens"  # listens[0-9]*
         objpropname = "listens"  # graphics.listens
-        objaddfn = "add_listen"  # graphics.add_listen
-        cb = self._make_find_inst_cb(cliarg, objpropname, objaddfn)
+        cb = self._make_find_inst_cb(cliarg, objpropname)
         return cb(*args, **kwargs)
 
     def _parse(self, inst):

virtinst/clock.py
@@ -36,10 +36,3 @@ class Clock(XMLBuilder):
 
     offset = XMLProperty("./@offset")
     timers = XMLChildProperty(_ClockTimer)
-
-    def add_timer(self):
-        obj = _ClockTimer(self.conn)
-        self.add_child(obj)
-        return obj
-    def remove_timer(self, obj):
-        self.remove_child(obj)

virtinst/cpu.py
@@ -43,11 +43,6 @@ class _CPUCell(XMLBuilder):
     memory = XMLProperty("./@memory", is_int=True)
     siblings = XMLChildProperty(_CPUCellSibling, relative_xpath="./distances")
 
-    def add_sibling(self):
-        obj = _CPUCellSibling(self.conn)
-        self.add_child(obj)
-        return obj
-
 
 class CPUCache(XMLBuilder):
     """
@@ -103,7 +98,7 @@ class CPU(XMLBuilder):
             self.vendor = None
             self.model_fallback = None
             for f in self.features:
-                self.remove_feature(f)
+                self.remove_child(f)
             self.mode = val
         elif val == self.SPECIAL_MODE_HOST_COPY:
             self.copy_host_cpu()
@@ -121,26 +116,13 @@ class CPU(XMLBuilder):
         self.special_mode_was_set = True
 
     def add_feature(self, name, policy="require"):
-        feature = CPUFeature(self.conn)
+        feature = self.features.add_new()
         feature.name = name
         feature.policy = policy
-
-        self.add_child(feature)
-    def remove_feature(self, feature):
-        self.remove_child(feature)
     features = XMLChildProperty(CPUFeature)
 
     cells = XMLChildProperty(_CPUCell, relative_xpath="./numa")
-    def add_cell(self):
-        obj = _CPUCell(self.conn)
-        self.add_child(obj)
-        return obj
-
     cache = XMLChildProperty(CPUCache)
-    def set_l3_cache_mode(self):
-        obj = CPUCache(self.conn)
-        self.add_child(obj)
-        return obj
 
     def copy_host_cpu(self):
         """
@@ -157,7 +139,7 @@ class CPU(XMLBuilder):
         self.vendor = cpu.vendor
 
         for feature in self.features:
-            self.remove_feature(feature)
+            self.remove_child(feature)
         for feature in cpu.features:
             self.add_feature(feature.name)
 

virtinst/cputune.py
@@ -34,7 +34,3 @@ class CPUTune(XMLBuilder):
     """
     _XML_ROOT_NAME = "cputune"
     vcpus = XMLChildProperty(_VCPUPin)
-    def add_vcpu(self):
-        obj = _VCPUPin(self.conn)
-        self.add_child(obj)
-        return obj

commit ad77f4fe83f2d9b63d1f6c1cd2c3a18374c53c92

createnet: fix typo introduced by commit dae8642bb
Signed-off-by: Pavel Hrdina <phrdina@redhat.com>

virtManager/createnet.py
@@ -740,7 +740,7 @@ class vmmCreateNetwork(vmmGObjectUI):
 
         if net.forward.mode == "hostdev":
             net.forward.managed = "yes"
-            pfobj = net.forward.pfs.add_new()
+            pfobj = net.forward.pf.add_new()
             pfobj.dev = net.forward.dev
             net.forward.dev = None
             net.domain_name = None