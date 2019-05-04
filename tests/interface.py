58d0d43db114dc55f8d8ce95c8ed1082b524ca6e
Cole Robinson <crobinso@redhat.com>
2019-05-04 12:13:37 -0400

    tests: Drop most interface XML testing
    No part of the app requires the <interface> details any more, so
    drop testing

--- tests/interface.py
+++ tests/interface.py
@@ -3,199 +3,30 @@
 # This work is licensed under the GNU GPLv2 or later.
 # See the COPYING file in the top-level directory.
 
-import os
 import unittest
-import logging
 
-from virtinst import Interface, InterfaceProtocol
+from virtinst import Interface
 from tests import utils
 
 datadir = "tests/interface-xml"
 
 
-
-
 class TestInterfaces(unittest.TestCase):
-    @property
-    def conn(self):
-        return utils.URIs.open_testdriver_cached()
+    def testInterfaceBridgeIP(self):
+        conn = utils.URIs.open_testdriver_cached()
 
-    def build_interface(self, interface_type, name):
-        iobj = Interface(self.conn)
-        iobj.type = interface_type
-        iobj.name = name
+        def _check_iface(name, typ, child_names):
+            libvirtobj = conn.interfaceLookupByName(name)
+            xmlobj = Interface(conn, parsexml=libvirtobj.XMLDesc(0))
 
-        return iobj
+            self.assertEqual(xmlobj.name, name)
+            self.assertEqual(xmlobj.type, typ)
+            self.assertEqual(
+                {i.name for i in xmlobj.interfaces},
+                set(child_names))
 
-    def set_general_params(self, iface_obj):
-        iface_obj.mtu = 1501
-        iface_obj.macaddr = "AA:AA:AA:AA:AA:AA"
-        iface_obj.start_mode = Interface.INTERFACE_START_MODE_ONBOOT
-        proto = InterfaceProtocol(self.conn)
-        proto.family = InterfaceProtocol.INTERFACE_PROTOCOL_FAMILY_IPV4
-        iface_obj.add_protocol(proto)
-
-    def add_child_interfaces(self, iface_obj):
-        def _m(_n):
-            xml = self.conn.interfaceLookupByName(_n).XMLDesc(0)
-            return Interface(self.conn, parsexml=xml)
-        if iface_obj.type == Interface.INTERFACE_TYPE_BRIDGE:
-            iface_obj.add_interface(_m("vlaneth1"))
-            iface_obj.add_interface(_m("bond-brbond"))
-            iface_obj.add_interface(_m("eth0"))
-        elif iface_obj.type == Interface.INTERFACE_TYPE_BOND:
-            iface_obj.add_interface(_m("eth0"))
-            iface_obj.add_interface(_m("eth1"))
-            iface_obj.add_interface(_m("eth2"))
-
-    def define_xml(self, obj, compare=True):
-        obj.validate()
-
-        xml = obj.get_xml()
-        logging.debug("Defining interface XML:\n%s", xml)
-
-        if compare:
-            filename = os.path.join(datadir, obj.name + ".xml")
-            utils.diff_compare(xml, filename)
-
-        iface = obj.install()
-
-        newxml = iface.XMLDesc(0)
-        logging.debug("Defined XML:\n%s", newxml)
-
-        iface.undefine()
-
-    # Bridge tests
-    def testBridgeInterface(self):
-        filename = "bridge"
-        obj = self.build_interface(Interface.INTERFACE_TYPE_BRIDGE,
-                                   "test-%s" % filename)
-        self.add_child_interfaces(obj)
-
-        obj.stp = False
-        obj.delay = "7"
-
-        self.define_xml(obj)
-
-    def testBridgeInterfaceIP(self):
-        filename = "bridge-ip"
-        obj = self.build_interface(Interface.INTERFACE_TYPE_BRIDGE,
-                                   "test-%s" % filename)
-        self.add_child_interfaces(obj)
-
-        # IPv4 proto
-        iface_proto1 = InterfaceProtocol(self.conn)
-        iface_proto1.family = InterfaceProtocol.INTERFACE_PROTOCOL_FAMILY_IPV4
-        iface_proto1.add_ip("129.63.1.2")
-        iface_proto1.add_ip("255.255.255.0")
-        iface_proto1.gateway = "1.2.3.4"
-        iface_proto1.dhcp = True
-        iface_proto1.dhcp_peerdns = True
-
-        # IPv6 proto
-        iface_proto2 = InterfaceProtocol(self.conn)
-        iface_proto2.family = InterfaceProtocol.INTERFACE_PROTOCOL_FAMILY_IPV6
-
-        iface_proto2.add_ip("fe99::215:58ff:fe6e:5", prefix="32")
-        iface_proto2.add_ip("fe80::215:58ff:fe6e:5", prefix="64")
-        iface_proto2.gateway = "1.2.3.4"
-        iface_proto2.dhcp = True
-        iface_proto2.dhcp_peerdns = True
-        iface_proto2.autoconf = True
-
-        obj.add_protocol(iface_proto1)
-        obj.add_protocol(iface_proto2)
-
-        self.define_xml(obj)
-
-    # Bond tests
-    def testBondInterface(self):
-        filename = "bond"
-        obj = self.build_interface(Interface.INTERFACE_TYPE_BOND,
-                                   "test-%s" % filename)
-        self.add_child_interfaces(obj)
-        self.set_general_params(obj)
-
-        self.define_xml(obj)
-
-    def testBondInterfaceARP(self):
-        filename = "bond-arp"
-        obj = self.build_interface(Interface.INTERFACE_TYPE_BOND,
-                                   "test-%s" % filename)
-        self.add_child_interfaces(obj)
-        self.set_general_params(obj)
-
-        obj.arp_interval = 100
-        obj.arp_target = "192.168.100.200"
-        obj.arp_validate_mode = "backup"
-
-        self.define_xml(obj)
-
-    def testBondInterfaceMII(self):
-        filename = "bond-mii"
-        obj = self.build_interface(Interface.INTERFACE_TYPE_BOND,
-                                   "test-%s" % filename)
-        self.add_child_interfaces(obj)
-        self.set_general_params(obj)
-
-        obj.mii_frequency = "123"
-        obj.mii_updelay   = "12"
-        obj.mii_downdelay = "34"
-        obj.mii_carrier_mode = "netif"
-
-        self.define_xml(obj)
-
-    # Ethernet tests
-    def testEthernetInterface(self):
-        filename = "ethernet"
-        obj = self.build_interface(Interface.INTERFACE_TYPE_ETHERNET,
-                                   "test-%s" % filename)
-        self.define_xml(obj)
-
-    def testEthernetManyParam(self):
-        filename = "ethernet-params"
-        obj = self.build_interface(Interface.INTERFACE_TYPE_ETHERNET,
-                                    "test-%s" % filename)
-
-        obj.mtu = 1234
-        obj.mac = "AA:BB:FF:FF:BB:AA"
-        obj.start_mode = Interface.INTERFACE_START_MODE_HOTPLUG
-
-        self.define_xml(obj)
-
-    # VLAN tests
-    def testVLANInterface(self):
-        filename = "vlan"
-        obj = self.build_interface(Interface.INTERFACE_TYPE_VLAN,
-                                   "test-%s" % filename)
-
-        obj.tag = "123"
-        obj.parent_interface = "eth2"
-
-        self.define_xml(obj)
-
-    def testVLANInterfaceBusted(self):
-        obj = self.build_interface(Interface.INTERFACE_TYPE_VLAN,
-                                   "vlan1")
-
-        try:
-            self.define_xml(obj, compare=False)
-            assert(False)
-        except ValueError:
-            pass
-        except Exception:
-            assert(False)
-
-    # protocol_xml test
-    def testEthernetProtocolInterface(self):
-        filename = "ethernet-copy-proto"
-        obj = self.build_interface(Interface.INTERFACE_TYPE_ETHERNET,
-                                   "test-%s" % filename)
-
-        protoxml = ("  <protocol family='ipv6'>\n"
-                    "    <dhcp/>\n"
-                    "  </protocol>\n")
-        proto = InterfaceProtocol(self.conn, parsexml=protoxml)
-        obj.add_protocol(proto)
-
-        self.define_xml(obj)
+        _check_iface("eth0", "ethernet", [])
+        _check_iface("bond0", "bond", ["eth-bond0-1", "eth-bond0-2"])
+        _check_iface("brplain", "bridge", ["eth-brplain0", "eth-brplain1"])
+        _check_iface("brempty", "bridge", [])
+        _check_iface("vlaneth1.3", "vlan", ["vlaneth1"])
