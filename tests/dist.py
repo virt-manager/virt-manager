8c5e0797e57cab4620d5db38908ea1837425630c
Cole Robinson <crobinso@redhat.com>
2019-06-17 13:30:35 -0400

    tests: dist: Drop forbidden import test
    It's broken, and in practice it has not turned out to be an issue
    preventing gdk or gtk from leaking into virtinst

--- tests/dist.py
+++ tests/dist.py
@@ -3,82 +3,15 @@
 # This work is licensed under the GNU GPLv2 or later.
 # See the COPYING file in the top-level directory.
 
-import fnmatch
 import glob
-import imp
-import importlib
-import os
-import sys
 import unittest
 import xml.etree.ElementTree as ET
-
-_badmodules = ["gi.repository.Gtk", "gi.repository.Gdk"]
-
-
-def _restore_modules(fn):
-    def wrap(*args, **kwargs):
-        origimport = __builtins__["__import__"]
-        def my_import(name, *iargs, **ikwargs):
-            if name in _badmodules:
-                raise AssertionError("Tried to import '%s'" % name)
-            return origimport(name, *iargs, **ikwargs)
-
-        try:
-            __builtins__["__import__"] = my_import
-            return fn(*args, **kwargs)
-        finally:
-            __builtins__["__import__"] = origimport
-    return wrap
-
-
-def _find_py(dirname):
-    ret = []
-    for root, ignore, filenames in os.walk(dirname):
-        for filename in fnmatch.filter(filenames, "*.py"):
-            ret.append(os.path.join(root, filename))
-    ret.sort(key=lambda s: s.lower())
-    return ret
 
 
 class TestDist(unittest.TestCase):
     """
     Tests to run before release
     """
-    def _check_modules(self, files):
-        for f in files:
-            regular_import = f.endswith(".py")
-            if f.endswith("/__init__.py"):
-                f = f.rsplit("/", 1)[0]
-            name = f.rsplit(".", 1)[0].replace("/", ".")
-            if name in sys.modules:
-                continue
-
-            if regular_import:
-                importlib.import_module(name)
-            else:
-                imp.load_source(name, f)
-
-        found = []
-        for f in _badmodules:
-            if f in sys.modules:
-                found.append(f)
-
-        if found:
-            raise AssertionError("%s found in sys.modules" % found)
-
-
-    @_restore_modules
-    def test_no_gtk_virtinst(self):
-        """
-        Make sure virtinst doesn't pull in any gnome modules
-        """
-        files = ["virt-install", "virt-clone", "virt-convert"]
-        files += _find_py("virtinst")
-        files += _find_py("virtconv")
-
-        self._check_modules(files)
-
-
     def test_validate_po_files(self):
         """
         Validate that po translations don't mess up python format strings,
