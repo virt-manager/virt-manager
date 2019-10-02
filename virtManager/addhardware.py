cdd1922a7184571054e884ab04ea56884429643e
Fabiano Fidêncio <fidencio@redhat.com>
2019-10-02 10:45:28 +0200

    addhardware: Add "bochs" display to the video list
    https://bugzilla.redhat.com/show_bug.cgi?id=1753644
    
    Signed-off-by: Fabiano Fidêncio <fidencio@redhat.com>

--- virtManager/addhardware.py
+++ virtManager/addhardware.py
@@ -663,7 +663,7 @@ class vmmAddHardware(vmmGObjectUI):
         if guest.conn.is_xen():
             return ["xen", "vga"]
         if guest.conn.is_qemu() or guest.conn.is_test():
-            return ["vga", "qxl", "virtio"]
+            return ["vga", "bochs", "qxl", "virtio"]
         return []
 
     @staticmethod
