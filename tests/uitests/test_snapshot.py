ad8a58bfcf80acf1baabddea072a17624967cb87
Cole Robinson <crobinso@redhat.com>
2020-08-23 16:56:05 -0400

    uitests: Finish snapshot.py coverage
    Signed-off-by: Cole Robinson <crobinso@redhat.com>

--- tests/uitests/test_snapshot.py
+++ tests/uitests/test_snapshot.py
@@ -48,26 +48,31 @@ class Snapshots(uiutils.UITestCase):
         snapname = "offline-root"
         win.find(snapname, "table cell").click()
         snaprun.click()
-        self._click_alert_button(
-                "sure you want to run the snapshot '%s'" % snapname,
-                "Yes")
+        self._click_alert_button("run the snapshot '%s'" % snapname, "No")
+        uiutils.check_in_loop(lambda: not vmrun.sensitive)
+        snaprun.click()
+        self._click_alert_button("run the snapshot '%s'" % snapname, "Yes")
         uiutils.check_in_loop(lambda: vmrun.sensitive)
 
         # Start paused snapshot
         snapname = "snap-paused"
         win.find(snapname, "table cell").click()
         snaprun.click()
-        self._click_alert_button(
-                "sure you want to run the snapshot '%s'" % snapname,
-                "Yes")
+        self._click_alert_button("run the snapshot '%s'" % snapname, "Yes")
         uiutils.check_in_loop(lambda: vmpause.checked)
 
         # Edit snapshot
-        desc = win.find("Description:", "text")
-        desc.text = "Test description foofoo"
+        descui = win.find("snapshot-description")
+        desc = "TESTSNAP"
+        descui.text = desc
         win.find("snapshot-apply", "push button").click()
         win.find("snapshot-refresh", "push button").click()
-        self.assertTrue("foofoo" in desc.text)
+        uiutils.check_in_loop(lambda: descui.text == desc)
+        # Apply by clicking away
+        desc += " ROUND2"
+        descui.text = desc
+        win.find("internal-root", "table cell").click()
+        self._click_alert_button("There are unapplied changes", "Yes")
 
         # Create new snapshot
         win.find("snapshot-add", "push button").click()
@@ -82,9 +87,97 @@ class Snapshots(uiutils.UITestCase):
 
         # Delete it
         win.find("snapshot-delete", "push button").click()
+        self._click_alert_button("permanently delete", "No")
+        uiutils.check_in_loop(lambda: not newc.dead)
+        win.find("snapshot-delete", "push button").click()
         self._click_alert_button("permanently delete", "Yes")
         uiutils.check_in_loop(lambda: newc.dead)
+
+        # Recreate another snapshot with the same name
+        win.find("snapshot-add", "push button").click()
+        newwin = self.app.root.find("Create snapshot", "frame")
+        snapname = "testnewsnap"
+        newwin.find("Name:", "text").text = snapname
+        newwin.find("Finish", "push button").click()
+        uiutils.check_in_loop(lambda: not newwin.showing)
+        newc = win.find(snapname, "table cell")
+        uiutils.check_in_loop(lambda: newc.state_selected)
 
         # Switch out of window
         win.find("Details", "radio button").click()
         uiutils.check_in_loop(lambda: not snaprun.showing)
+
+    def testSnapshotMisc1(self):
+        """
+        Test snapshot corner cases
+        """
+        manager = self.app.topwin
+        manager.find("vm-list").click()
+        vmname = "test-state-managedsave"
+        cell = manager.find_fuzzy(vmname, "table cell")
+        cell.bring_on_screen()
+
+        win = self._open_snapshots_window(vmname=vmname)
+        vmrun = win.find("Restore", "push button")
+
+        # Create new snapshot
+        win.find("snapshot-add", "push button").click()
+        self._click_alert_button("not become part of the snapshot", "Cancel")
+        uiutils.check_in_loop(lambda: win.active)
+        win.find("snapshot-add", "push button").click()
+        self._click_alert_button("not become part of the snapshot", "OK")
+        newwin = self.app.root.find("Create snapshot", "frame")
+        snapname1 = "testnewsnap1"
+        newwin.find("Name:", "text").text = snapname1
+        newwin.find("Finish", "push button").click()
+        uiutils.check_in_loop(lambda: not newwin.showing)
+        newc = win.find(snapname1, "table cell")
+        uiutils.check_in_loop(lambda: newc.state_selected)
+
+        # Start the VM, create another snapshot
+        vmrun.click()
+        uiutils.check_in_loop(lambda: not vmrun.sensitive)
+        win.find("snapshot-add", "push button").click()
+        newwin = self.app.root.find("Create snapshot", "frame")
+        # Force validation error
+        newwin.find("Name:", "text").text = "bad name"
+        newwin.find("Finish", "push button").click()
+        self._click_alert_button("validating snapshot", "OK")
+        # Force name collision
+        newwin.find("Name:", "text").text = snapname1
+        newwin.find("Finish", "push button").click()
+        self._click_alert_button(snapname1, "Close")
+        # Make it succeed
+        snapname2 = "testnewsnap2"
+        newwin.find("Name:", "text").text = snapname2
+        newwin.find("Finish", "push button").click()
+        uiutils.check_in_loop(lambda: not newwin.showing)
+        newc = win.find(snapname2, "table cell")
+        uiutils.check_in_loop(lambda: newc.state_selected)
+
+        # Trigger another managed save warning
+        smenu = win.find("Menu", "toggle button")
+        smenu.click()
+        save = smenu.find("Save", "menu item")
+        save.click()
+        uiutils.check_in_loop(lambda: vmrun.sensitive)
+        win.find(snapname1, "table cell").click(button=3)
+        self.app.root.find("Start snapshot", "menu item").click()
+        self._click_alert_button("run the snapshot '%s'" % snapname1, "Yes")
+        self._click_alert_button("no memory state", "Cancel")
+        win.find("snapshot-start").click()
+        self._click_alert_button("run the snapshot '%s'" % snapname1, "Yes")
+        self._click_alert_button("no memory state", "OK")
+
+        # Multi select
+        cell1 = win.find(snapname1, "table cell")
+        cell2 = win.find(snapname2, "table cell")
+        cell1.click()
+        self.holdKey("Shift_L")
+        self.pressKey("Down")
+        self.releaseKey("Shift_L")
+        win.find("snapshot-delete").click()
+        self._click_alert_button("permanently delete", "Yes")
+        uiutils.check_in_loop(lambda: cell1.dead)
+        uiutils.check_in_loop(lambda: cell2.dead)
+        uiutils.check_in_loop(lambda: win.active)
