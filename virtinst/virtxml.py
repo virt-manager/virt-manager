virt-manager/virt-manager

commit 71f034d6b6aad1c8d08b6e95496327299fe797e9

i18n: fix string puzzles in error messages
Do not split the error messages and the error details, but rather use a
single string with proper placeholders. This avoids string puzzles.

Reviewed-by: Cole Robinson <crobinso@redhat.com>
Signed-off-by: Pino Toscano <ptoscano@redhat.com>

virtinst/virtxml.py
@@ -243,8 +243,10 @@ def start_domain_transient(conn, xmlobj, devs, action, confirm):
     try:
         dom = conn.createXML(xmlobj.get_xml())
     except libvirt.libvirtError as e:
-        fail((_("Failed starting domain '%s'") % xmlobj.name) +
-             (": %s" % e))
+        fail(_("Failed starting domain '%(domain)s': %(error)s") % {
+                 "vm": xmlobj.name,
+                 "error": e,
+             })
     else:
         print_stdout(_("Domain '%s' started successfully.") % xmlobj.name)
         return dom
@@ -278,8 +280,10 @@ def update_changes(domain, devs, action, confirm):
             elif action == "update":
                 domain.updateDeviceFlags(xml, libvirt.VIR_DOMAIN_AFFECT_LIVE)
         except libvirt.libvirtError as e:
-            fail((_("Error attempting device action %s") % action) +
-                 (": %s" % e))
+            fail(_("Error attempting device action %(action)s: %(error)s") % {
+                     "action": action,
+                     "error": e,
+                 })
 
         # Test driver doesn't support device hotplug so we can't reach this
         print_stdout(_("Device %s successful.") % action)  # pragma: no cover
@@ -505,8 +509,10 @@ def main(conn=None):
         try:
             dom.create()
         except libvirt.libvirtError as e:  # pragma: no cover
-            fail((_("Failed starting domain '%s'") % inactive_xmlobj.name) +
-                 (": " % e))
+            fail(_("Failed starting domain '%(domain)s': %(error)s") % {
+                     "domain": inactive_xmlobj.name,
+                     "error": e,
+                 })
         print_stdout(_("Domain '%s' started successfully.") %
                      inactive_xmlobj.name)
 

virtManager/clone.py
@@ -806,10 +806,12 @@ class vmmCloneVM(vmmGObjectUI):
         self.reset_finish_cursor()
 
         if error is not None:
-            msg = (_("Error creating virtual machine clone '%s'") %
-                      self.clone_design.clone_name)
-            msg += ": %s" % error
-            self.err.show_err(msg, details=details)
+            error = (_("Error creating virtual machine clone '%(vm)s': "
+                       "%(error)s") % {
+                     "vm": self.clone_design.clone_name,
+                     "error": error,
+                     })
+            self.err.show_err(error, details=details)
             return
 
         conn.schedule_priority_tick(pollvm=True)

virtManager/connection.py
@@ -598,11 +598,16 @@ class vmmConnection(vmmGObject):
             except Exception as fixerr:
                 log.debug("Failed to redefine original %s!",
                     obj.class_name(), exc_info=True)
-                msg = (_("%s rename failed. Attempting to "
-                    "recover also failed") % (obj.class_name()))
-                msg += "\n\n"
-                msg += ("Original error: %s\n\n" % str(renameerr))
-                msg += ("Recover error: %s" % str(fixerr))
+                msg = _("%(object)s rename failed. Attempting to recover also "
+                        "failed.\n"
+                        "\n"
+                        "Original error: %(origerror)s\n"
+                        "\n"
+                        "Recover error: %s") % {
+                            "object": obj.class_name(),
+                            "origerror": str(renameerr),
+                            "recovererror": str(fixerr),
+                        }
                 raise RuntimeError(msg)
             raise
         finally:

virtManager/createvm.py
@@ -1923,7 +1923,7 @@ class vmmCreateVM(vmmGObjectUI):
             self._show_customize_dialog(guest, installer)
         except Exception as e:
             self.reset_finish_cursor()
-            self.err.show_err(_("Error starting installation: ") + str(e))
+            self.err.show_err(_("Error starting installation: %s") % str(e))
             return
 
     def _cleanup_customize_window(self):

virtManager/delete.py
@@ -199,9 +199,10 @@ class _vmmDeleteBase(vmmGObjectUI):
 
             self._delete_vm(vm)
         except Exception as e:
-            error = (
-                (_("Error deleting virtual machine '%s'") % vm.get_name()) +
-                (": %s") % str(e))
+            error = _("Error deleting virtual machine '%(vm)s': %(error)s") % {
+                        "vm": vm.get_name(),
+                        "error": str(e),
+                    }
             details = "".join(traceback.format_exc())
 
         storage_errstr = ""

virtManager/details/console.py
@@ -736,7 +736,7 @@ class vmmConsolePages(vmmGObjectUI):
         except Exception as e:
             log.exception("Error connection to graphical console")
             self._activate_unavailable_page(
-                    _("Error connecting to graphical console") + ":\n%s" % e)
+                    _("Error connecting to graphical console:\n%s") % e)
 
     def _set_credentials(self, src_ignore=None):
         passwd = self.widget("console-auth-password")

virtManager/details/viewers.py
@@ -461,8 +461,10 @@ class VNCViewer(Viewer):
             self._sockfd = sock
         except Exception as e:
             raise RuntimeError(
-                (_("Error opening socket path '%s'") % self._ginfo.gsocket) +
-                (": %s" % e))
+                _("Error opening socket path '%(path)s': %(error)s") % {
+                    "path": self._ginfo.gsocket,
+                    "error": e,
+                })
 
         fd = self._sockfd.fileno()
         if fd < 0:

virtManager/device/netlist.py
@@ -211,8 +211,10 @@ class vmmNetworkList(vmmGObjectUI):
             log.debug("Started network '%s'", devname)
         except Exception as e:
             return self.err.show_err(
-                    (_("Could not start virtual network '%s'") % devname) +
-                    (": %s") % str(e))
+                _("Could not start virtual network '%(device)s': %(error)s") % {
+                    "device": devname,
+                    "error": str(e),
+                })
 
     def _find_rowiter_for_dev(self, net):
         nettype = net.type

commit 143c6befc33ee507379fd7eca8cf5e5bd1685799

virtinst: fix error message format string
Fix a regression introduced by commit 71f034d6b where
format string expects kwarg "domain", but "vm" is passed instead.

Reviewed-by: Cole Robinson <crobinso@redhat.com>
Signed-off-by: Roman Bogorodskiy <bogorodskiy@gmail.com>

virtinst/virtxml.py
@@ -251,7 +251,7 @@ def start_domain_transient(conn, xmlobj, devs, action, confirm):
         dom = conn.createXML(xmlobj.get_xml())
     except libvirt.libvirtError as e:
         fail(_("Failed starting domain '%(domain)s': %(error)s") % {
-                 "vm": xmlobj.name,
+                 "domain": xmlobj.name,
                  "error": e,
              })
     else: