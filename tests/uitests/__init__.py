a8e978dd388f94164e562303395ff3812e79edae
Cole Robinson <crobinso@redhat.com>
2021-05-22 13:26:32 -0400

    pylint: Another fix for gtk3 version detection
    We need to place require_version calls earlier and before
    any code that might import Gtk/Gdk
    
    Signed-off-by: Cole Robinson <crobinso@redhat.com>

--- tests/uitests/__init__.py
+++ tests/uitests/__init__.py
@@ -1,2 +1,7 @@
 # This work is licensed under the GNU GPLv2 or later.
 # See the COPYING file in the top-level directory.
+
+import gi
+
+gi.require_version("Gdk", "3.0")
+gi.require_version("Gtk", "3.0")
