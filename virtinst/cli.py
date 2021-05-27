7e5ce503fe18d6dce421cf32f152d4cc3d63a127
Han Han <hhan@redhat.com>
2021-05-27 12:01:48 +0800

    cli: --feature: Support kvm.poll-control.state
    Implement kvm.poll-control.state to `virt-install --feature`. It requires
    libvirt >= v6.10.0.
    
    Reviewed-by: Cole Robinson <crobinso@redhat.com>
    Signed-off-by: Han Han <hhan@redhat.com>

--- virtinst/cli.py
+++ virtinst/cli.py
@@ -2782,6 +2782,7 @@ class ParserFeatures(VirtCLIParser):
         cls.add_arg("vmport.state", "vmport", is_onoff=True)
         cls.add_arg("kvm.hidden.state", "kvm_hidden", is_onoff=True)
         cls.add_arg("kvm.hint-dedicated.state", "kvm_hint_dedicated", is_onoff=True)
+        cls.add_arg("kvm.poll-control.state", "kvm_poll_control", is_onoff=True)
         cls.add_arg("pvspinlock.state", "pvspinlock", is_onoff=True)
 
         cls.add_arg("gic.version", "gic_version")
