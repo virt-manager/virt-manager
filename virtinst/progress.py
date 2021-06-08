078178f476fae11e43693cbe0e7ff049012d73cc
Cole Robinson <crobinso@redhat.com>
2021-06-08 18:31:44 -0400

    progress: Drop tqdm and revert back to local urlgrabber copy
    I thought tqdm was available everywhere, but it does not seem like
    it will be in RHEL9. Revert back to the old urlgrabber copy, now
    stored in virtinst/_progresspriv.py. If we ever want to try tqdm
    again, we can just revert this commit
    
    Signed-off-by: Cole Robinson <crobinso@redhat.com>

--- virtinst/progress.py
+++ virtinst/progress.py
@@ -5,59 +5,41 @@
 # See the COPYING file in the top-level directory.
 #
 
-import tqdm
+import sys
 
-
-class _fakewriter:
-    def write(self, msg):
-        pass
-    def flush(self):
-        pass
+from . import _progresspriv
 
 
 class Meter:
     """
-    Compat API that matches what urlgrabber used to provide, so our
-    internal callers don't need to adapter
+    Meter class that hides the internals of the backend implementation
+    from virtinst and friends
     """
-    def __init__(self, quiet=False, bar_format=None):
+    # Used by virt-manager subclass
+    format_number = _progresspriv.format_number
+    format_time = _progresspriv.format_time
+
+    def __init__(self, quiet=False):
         self._text = None
         self._size = None
         self._total_read = 0
-        self._tqdm = None
-        self._quiet = quiet
-        self._bar_format = bar_format
+        if quiet:
+            self._meter = _progresspriv.BaseMeter()
+        else:
+            self._meter = _progresspriv.TextMeter(output=sys.stdout)
 
     def start(self, text, size):
         self._text = text
         self._size = size
         self._total_read = 0
-
-        if self._quiet:
-            fileobj = _fakewriter()
-        else:
-            fileobj = None
-
-        self._tqdm = tqdm.tqdm(
-                desc=self._text, total=self._size,
-                unit='B', unit_scale=True, unit_divisor=1024,
-                miniters=0, mininterval=0.25, leave=True, file=fileobj,
-                # Set bar_format to only print the text to start. We
-                # don't want it to print bytes info until first data comes in
-                bar_format="{desc}",
-        )
+        self._meter.start(text, size)
 
     def update(self, new_total):
-        update_amount = new_total - self._total_read
         self._total_read = new_total
-        # Data came in, use our requested bar_format
-        self._tqdm.bar_format = self._bar_format
-        self._tqdm.update(update_amount)
+        self._meter.update(new_total)
 
     def end(self):
-        if self._tqdm is not None:
-            self._tqdm.close()
-        self._tqdm = None
+        self._meter.end()
 
 
 def make_meter(quiet):
