193df29376fdf27e6a80c0894abb05116538a2e7
Cole Robinson <crobinso@redhat.com>
2022-02-16 13:54:41 -0500

    guest: Default to 16 pcie root ports for q35
    Add extra PCIe root ports to enable q35 device hotplug to work out
    of the box. A typical modern linux guest has 7-8 PCI devices added
    by default, so this gives plenty of wiggle room.
    
    The smart thing to do would be to count the attached PCI devices
    and add 4-5 extra, but that takes more work and isn't trivial.
    
    The number can be overridden on the cli with:
    
      --controller q35_pcie_root_ports=X
    
    Use =0 to go back to the old behavior.
    
    Signed-off-by: Cole Robinson <crobinso@redhat.com>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1016,8 +1016,8 @@ c.add_compare("--disk %(EXISTIMG1)s --install bootdev=network --os-variant rhel5
 c.add_compare("--disk %(EXISTIMG1)s --install kernel=%(ISO-WIN7)s,initrd=%(ISOLABEL)s,kernel_args='foo bar' --os-variant rhel6.4 --unattended none", "kvm-rhel6")  # RHEL6 defaults. ISO paths are just to point at existing files
 c.add_compare("--disk %(EXISTIMG1)s --location https://example.com --install kernel_args='test overwrite',kernel_args_overwrite=yes --os-variant rhel7.0", "kvm-rhel7", precompare_check=no_osinfo_unattend_cb)  # RHEL7 defaults
 c.add_compare("--connect " + utils.URIs.kvm_x86_nodomcaps + " --disk %(EXISTIMG1)s --pxe --os-variant rhel7.0", "kvm-cpu-default-fallback", prerun_check=has_old_osinfo)  # No domcaps, so mode=host-model isn't safe, so we fallback to host-model-only
-c.add_compare("--disk %(EXISTIMG1)s --pxe --os-variant centos7.0", "kvm-centos7", prerun_check=has_old_osinfo)  # Centos 7 defaults
-c.add_compare("--disk %(EXISTIMG1)s --cdrom %(EXISTIMG2)s --os-variant win10", "kvm-win10", prerun_check=has_old_osinfo)  # win10 defaults
+c.add_compare("--disk %(EXISTIMG1)s --pxe --os-variant centos7.0 --controller q35_pcie_root_ports=0", "kvm-centos7", prerun_check=has_old_osinfo)  # Centos 7 defaults
+c.add_compare("--disk %(EXISTIMG1)s --cdrom %(EXISTIMG2)s --os-variant win10 --controller q35_pcie_root_ports=2", "kvm-win10", prerun_check=has_old_osinfo)  # win10 defaults
 c.add_compare("--os-variant win7 --cdrom %(EXISTIMG2)s --boot loader_type=pflash,loader=CODE.fd,nvram_template=VARS.fd --disk %(EXISTIMG1)s", "win7-uefi", prerun_check=has_old_osinfo)  # no HYPER-V with UEFI
 c.add_compare("--osinfo generic --arch i686 --boot uefi --install kernel=http://example.com/httpkernel,initrd=ftp://example.com/ftpinitrd --disk none", "kvm-i686-uefi")  # i686 uefi. piggy back it for --install testing too
 c.add_compare("--osinfo generic --machine q35 --cdrom %(EXISTIMG2)s --disk %(EXISTIMG1)s", "q35-defaults")  # proper q35 disk defaults
