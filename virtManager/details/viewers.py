9db16377e89e0cb54ce0051b65c9e521cf96f06b
Cole Robinson <crobinso@redhat.com>
2022-03-01 14:52:26 -0500

    viewers: Fix import error when spice is missing
    Signed-off-by: Cole Robinson <crobinso@redhat.com>

--- virtManager/details/viewers.py
+++ virtManager/details/viewers.py
@@ -21,8 +21,8 @@ try:
     gi.require_version('SpiceClientGtk', '3.0')
     from gi.repository import SpiceClientGtk
     from gi.repository import SpiceClientGLib
-except (ValueError, ImportError) as SPICE_GTK_IMPORT_ERROR:
-    SPICE_GTK_IMPORT_ERROR = str(SPICE_GTK_IMPORT_ERROR)
+except (ValueError, ImportError) as _SPICE_GTK_IMPORT_ERROR:
+    SPICE_GTK_IMPORT_ERROR = str(_SPICE_GTK_IMPORT_ERROR)
 
 from virtinst import log
 
