0b9e864467aee99f46227b637b76125dc6d4093d
Jonathon Jongsma <jjongsma@redhat.com>
2022-12-01 14:50:43 -0600

    Handle media that matches more than one OS
    Some ISO files (especially windows versions) don't have a way to
    reliably differentiate between different versions. To deal with this,
    libosinfo recently added some new APIs to return a list of all OSes that
    match a given media source. Switch to these new APIs when available and
    only auto-detect the OS if there is no ambiguity (i.e. only one match is
    returned).
    
    Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=2141995
    
    Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>

--- virtinst/osdict.py
+++ virtinst/osdict.py
@@ -16,6 +16,20 @@ from . import xmlutil
 from .logger import log
 
 
+def _process_medialist(medialist):
+    nmedias = medialist.get_length()
+    if nmedias == 1:
+        return medialist.get_nth(0)
+
+    # can't determine a single definitive match. Print the possible matches and return None
+    if nmedias > 1:
+        ids = []
+        for n in range(nmedias):
+            ids.append(medialist.get_nth(n).get_os().get_short_id())
+        log.debug("media matches multiple operating systems: %s", ", ".join(ids))
+
+    return None
+
 def _media_create_from_location(location):
     if not hasattr(Libosinfo.Media, "create_from_location_with_flags"):
         return Libosinfo.Media.create_from_location(  # pragma: no cover
@@ -119,8 +133,16 @@ class _OSDB(object):
             log.debug("Error creating libosinfo media object: %s", str(e))
             return None
 
-        if not self._os_db.identify_media(media):
-            return None
+        if hasattr(self._os_db, "identify_medialist"):
+            # osinfo_db_identify_medialist is part of libosinfo 1.10.0
+            medialist = self._os_db.identify_medialist(media)
+            media = _process_medialist(medialist);
+            if media is None:
+                return None
+        else:
+            if not self._os_db.identify_media(media):
+                return None
+
         return media.get_os().get_short_id(), _OsMedia(media)
 
     def guess_os_by_tree(self, location):
@@ -139,6 +161,13 @@ class _OSDB(object):
                 "location=%s : %s", location, str(e))
             return None
 
+        if hasattr(self._os_db, "identify_treelist"):
+            # osinfo_db_identify_treelist is part of libosinfo 1.10.0
+            treelist = self._os_db.identify_treelist(tree)
+            tree = _process_medialist(treelist)
+            if tree is None:
+                return None
+            return tree.get_os().get_short_id(), _OsTree(tree)
         if hasattr(self._os_db, "identify_tree"):
             # osinfo_db_identify_tree is part of libosinfo 1.6.0
             if not self._os_db.identify_tree(tree):
