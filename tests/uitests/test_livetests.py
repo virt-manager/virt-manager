75422ec75efe254beb8dc85f75715d71fa3ec859
Cole Robinson <crobinso@redhat.com>
2023-05-06 13:13:22 -0400

    uitests: Make hotplug test pass on both f37 and f38
    Signed-off-by: Cole Robinson <crobinso@redhat.com>

--- tests/uitests/test_livetests.py
+++ tests/uitests/test_livetests.py
@@ -471,11 +471,15 @@ def _testLiveHotplug(app, fname):
     lib.utils.check(lambda: tab.showing)
     entry.set_text(fname)
     appl.click()
-    # F38 CDROM change is broken:
-    # https://gitlab.com/qemu-project/qemu/-/issues/933
     # pylint: disable=unreachable
-    app.click_alert_button("changes will take effect", "OK")
-    return
+    import dogtail.tree
+    try:
+        # F38 CDROM change is broken:
+        # https://gitlab.com/qemu-project/qemu/-/issues/933
+        app.click_alert_button("changes will take effect", "OK")
+        return
+    except dogtail.tree.SearchError:
+        pass
 
     lib.utils.check(lambda: not appl.sensitive)
     lib.utils.check(lambda: entry.text == fname)
