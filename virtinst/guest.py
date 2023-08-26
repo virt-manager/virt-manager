virt-manager/virt-manager

commit 05fcc7410eee0dc90331b49d0a7e7727f362c2e4

virtinst: fix caching of domain capabilities
The domain capabilities XML always contains the canonical machine name.
When the selected machine type for a guest is an alias, the check
comparing it to the domain capabilities machine will always fail. This
prevents the domain capabilities result from being cached.

Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

virtinst/guest.py
@@ -618,13 +618,21 @@ class Guest(XMLBuilder):
         return True
 
     def lookup_domcaps(self):
+        def _compare_machine(domcaps):
+            capsinfo = self.lookup_capsinfo()
+            if self.os.machine == domcaps.machine:
+                return True
+            if capsinfo.is_machine_alias(self.os.machine, domcaps.machine):
+                return True
+            return False
+
         # We need to regenerate domcaps cache if any of these values change
         def _compare(domcaps):  # pragma: no cover
             if self.type == "test":
                 # Test driver doesn't support domcaps. We kinda fake it in
                 # some cases, but it screws up the checking here for parsed XML
                 return True
-            if self.os.machine and self.os.machine != domcaps.machine:
+            if self.os.machine and not _compare_machine(domcaps):
                 return False
             if self.type and self.type != domcaps.domain:
                 return False

virtinst/capabilities.py
@@ -129,6 +129,19 @@ class _CapsGuest(XMLBuilder):
                 ret.append(m.canonical)
         return ret
 
+    def is_machine_alias(self, domain, src, tgt):
+        """
+        Determine if machine @src is an alias for machine @tgt
+        """
+        mobjs = (domain and domain.machines) or self.machines
+        for m in mobjs:
+            log.debug("Check %s == %s %s == %s", m.name, src, m.canonical, tgt)
+            if m.name == src and m.canonical == tgt:
+                log.debug("ok")
+                return True
+        log.debug("not ok")
+        return False
+
     def is_kvm_available(self):
         """
         Return True if kvm guests can be installed
@@ -178,6 +191,9 @@ class _CapsInfo(object):
 
         self.emulator = self.domain.emulator or self.guest.emulator
         self.machines = self.guest.all_machine_names(self.domain)
+
+    def is_machine_alias(self, src, tgt):
+        return self.guest.is_machine_alias(self.domain, src, tgt)
 
 
 class Capabilities(XMLBuilder):

commit 2f50cab0ccca61bd8c087ab24b9c8bcc22e2fdb6

virtinst: delay "lookup_capsinfo" until we really need it
When I try to open the details window for a domain that does not use the
system default emulator, I get the following exception:

> Traceback (most recent call last):
>   File "virtManager/vmwindow.py", line 40, in get_instance
>     cls._instances[key] = vmmVMWindow(vm)
>   File "virtManager/vmwindow.py", line 83, in __init__
>     self._details = vmmDetails(self.vm, self.builder, self.topwin,
>   File "virtManager/details/details.py", line 389, in __init__
>     self._init_details()
>   File "virtManager/details/details.py", line 807, in _init_details
>     vmmAddHardware.build_video_combo(self.vm, video_dev)
>   File "virtManager/addhardware.py", line 816, in build_video_combo
>     default = DeviceVideo.default_model(vm.xmlobj)
>   File "virtinst/devices/video.py", line 47, in default_model
>     if (guest.lookup_domcaps().supports_video_virtio() and
>   File "virtinst/guest.py", line 656, in lookup_domcaps
>     if not self._domcaps or not _compare(self._domcaps):
>   File "virtinst/guest.py", line 646, in _compare
>     if self.os.machine and not _compare_machine(domcaps):
>   File "virtinst/guest.py", line 633, in _compare_machine
>     capsinfo = self.lookup_capsinfo()
>   File "virtinst/guest.py", line 674, in lookup_capsinfo
>     self._capsinfo = self.conn.caps.guest_lookup(
>   File "virtinst/capabilities.py", line 319, in guest_lookup
>     raise ValueError(msg)
> ValueError: Host does not support domain type kvm with machine
> 'pc-q35-8.1' for virtualization type 'hvm' with architecture 'x86_64'

This is a regression; according to git-bisect, it was introduced in commit
05fcc7410eee ("virtinst: fix caching of domain capabilities", 2022-07-27).

"lookup_capsinfo" (and "guest_lookup" called by it) are unsuitable for
machine type alias checking (or for anything else) if the domain uses an
emulator that differs from the system default emulator. The information

virtinst/guest.py
@@ -630,9 +630,9 @@ class Guest(XMLBuilder):
 
     def lookup_domcaps(self):
         def _compare_machine(domcaps):
-            capsinfo = self.lookup_capsinfo()
             if self.os.machine == domcaps.machine:
                 return True
+            capsinfo = self.lookup_capsinfo()
             if capsinfo.is_machine_alias(self.os.machine, domcaps.machine):
                 return True
             return False