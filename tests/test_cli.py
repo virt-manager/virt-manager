0ba63f94c1df4979cf36dac5531f097ad6be192b
Andrea Bolognani <abologna@redhat.com>
2024-05-22 18:45:09 +0200

    tests: Drop precompare_check from riscv64
    We're using data from a much newer version of libvirt now.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1166,8 +1166,8 @@ c.add_compare("--connect %(URI-KVM-S390X)s --arch s390x --nographics --import --
 # riscv tests #
 ###############
 
-c.add_compare("--connect %(URI-QEMU-RISCV64)s --arch riscv64 --os-variant fedora29 --import --disk %(EXISTIMG1)s --network default --graphics none", "riscv64-headless", precompare_check="5.3.0")
-c.add_compare("--connect %(URI-QEMU-RISCV64)s --arch riscv64 --os-variant fedora29 --import --disk %(EXISTIMG1)s --network default --graphics vnc", "riscv64-graphics", precompare_check="5.3.0", )
+c.add_compare("--connect %(URI-QEMU-RISCV64)s --arch riscv64 --os-variant fedora29 --import --disk %(EXISTIMG1)s --network default --graphics none", "riscv64-headless")
+c.add_compare("--connect %(URI-QEMU-RISCV64)s --arch riscv64 --os-variant fedora29 --import --disk %(EXISTIMG1)s --network default --graphics vnc", "riscv64-graphics")
 
 
 
