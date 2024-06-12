ab9f527c9a5052fb889d135d8362c4c7b78f7d92
Pavel Hrdina <phrdina@redhat.com>
2024-06-12 11:55:34 +0200

    virtinst: add support for hyperv_tblflush feature
    Signed-off-by: Pavel Hrdina <phrdina@redhat.com>

--- virtinst/cli.py
+++ virtinst/cli.py
@@ -3018,6 +3018,7 @@ class ParserFeatures(VirtCLIParser):
         cls.add_arg("hyperv.reset.state", "hyperv_reset", is_onoff=True)
         cls.add_arg("hyperv.frequencies.state", "hyperv_frequencies", is_onoff=True)
         cls.add_arg("hyperv.reenlightenment.state", "hyperv_reenlightenment", is_onoff=True)
+        cls.add_arg("hyperv.tlbflush.state", "hyperv_tlbflush", is_onoff=True)
 
         cls.add_arg("vmport.state", "vmport", is_onoff=True)
         cls.add_arg("kvm.hidden.state", "kvm_hidden", is_onoff=True)
