17f171cdb73f45771138eca4ac2c39068ba96f6b
Cole Robinson <crobinso@redhat.com>
2024-11-16 15:26:28 -0500

    cli: add `--network passt,portForward=8080:80` convenience syntax
    Roughly mirroring `podman run -p` syntax. Examples:
    
    --network passt,portForward=8080:80
    --network passt,portForward0=7000-8000/udp,portForward1=127.0.0.1:2222:22
    
    Resolves: https://github.com/virt-manager/virt-manager/issues/751
    
    Signed-off-by: Cole Robinson <crobinso@redhat.com>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -653,6 +653,10 @@ source.reservations.managed=no,source.reservations.source.type=unix,source.reser
 --network user,address.type=ccw,address.cssid=0xfe,address.ssid=0,address.devno=01,boot.order=15,boot.loadparm=SYSTEM1
 --network model=vmxnet3
 --network backend.type=passt,backend.logFile=/tmp/foo.log,portForward0.proto=tcp,portForward0.address=192.168.10.10,portForward0.dev=eth0,portForward0.range0.start=4000,portForward0.range0.end=5000,portForward0.range0.to=10000,portForward0.range0.exclude=no,portForward0.range1.start=6000,portForward1.proto=tcp,portForward1.range0.start=2022,portForward1.range0.to=22
+--network passt,portForward=8080:80
+--network passt,portForward=8080
+--network passt,portForward0=7000-8000/udp,portForward1=127.0.0.1:2222:22
+--network passt,portForward0=2001:db8:ac10:fd01::1:10:3000-4000:30,portForward1=127.0.0.1:5000-6000:5
 --network type=hostdev,source.address.type=pci,source.address.domain=0x0,source.address.bus=0x00,source.address.slot=0x07,source.address.function=0x0
 --network hostdev=pci_0000_00_09_0
 --network hostdev=0:0:4.0
