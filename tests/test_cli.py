36d00e0e79f3d845522201c4a353d5a529af82a7
Lin Ma <lma@suse.de>
2025-01-05 17:49:34 +0800

    cli: Add --disk driver.queue_size support
    Eg:
    virt-install \
    ......
    --disk /tmp/disk0.qcow2,size=10,driver.type=qcow2,\
    driver.queues=4,driver.queue_size=256 \
    ......
    
    It results in the following domain XML snippet:
        <disk type='file' device='disk'>
          <driver name='qemu' type='qcow2' queues='4' queue_size='256'/>
          <source file='/tmp/disk0.qcow2' index='2'/>
          <backingStore/>
          <target dev='vda' bus='virtio'/>
          <address type='pci' domain='0x0000' bus='0x05' slot='0x00' function='0x0'/>
        </disk>
    
    Signed-off-by: Lin Ma <lma@suse.de>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -610,7 +610,7 @@ msrs.unknown=ignore
 --sysinfo bios.vendor="Acme LLC",bios.version=1.2.3,bios.date=01/01/1970,bios.release=10.22,system.manufacturer="Acme Inc.",system.product=Computer,system.version=3.2.1,system.serial=123456789,system.uuid=00000000-1111-2222-3333-444444444444,system.sku=abc-123,system.family=Server,baseBoard.manufacturer="Acme Corp.",baseBoard.product=Motherboard,baseBoard.version=A01,baseBoard.serial=1234-5678,baseBoard.asset=Tag,baseBoard.location=Chassis
 
 
---disk type=block,source.dev=/pool-dir/UPPER,cache=writeback,io=threads,perms=sh,serial=WD-WMAP9A966149,wwn=123456789abcdefa,boot_order=2,driver.iothread=3,driver.queues=8
+--disk type=block,source.dev=/pool-dir/UPPER,cache=writeback,io=threads,perms=sh,serial=WD-WMAP9A966149,wwn=123456789abcdefa,boot_order=2,driver.iothread=3,driver.queues=8,driver.queue_size=256
 --disk source.file=%(NEWIMG1)s,sparse=false,size=.001,perms=ro,error_policy=enospace,detect_zeroes=unmap,address.type=drive,address.controller=0,address.target=2,address.unit=0
 --disk device=cdrom,bus=sata,read_bytes_sec=1,read_iops_sec=2,write_bytes_sec=5,write_iops_sec=6,driver.copy_on_read=on,geometry.cyls=16383,geometry.heads=16,geometry.secs=63,geometry.trans=lba,discard=ignore
 --disk size=1
