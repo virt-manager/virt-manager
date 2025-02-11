313120c9e7fd215dac9561fec18999c48a0c8280
Lin Ma <lma@suse.de>
2025-02-11 10:56:12 +0800

    tests: add pstore test
    Signed-off-by: Lin Ma <lma@suse.de>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -799,6 +799,9 @@ source.reservations.managed=no,source.reservations.source.type=unix,source.reser
 --vsock cid=17
 
 
+--pstore backend=acpi-erst,path=/tmp/guest_acpi_esrt,size=8
+
+
 --tpm passthrough,model=tpm-crb,path=/dev/tpm0,backend.encryption.secret=11111111-2222-3333-4444-5555555555,backend.persistent_state=yes,backend.active_pcr_banks.sha1=on,backend.active_pcr_banks.sha256=yes,backend.active_pcr_banks.sha384=yes,backend.active_pcr_banks.sha512=yes,version=2.0
 
 --tpm model=tpm-tis,backend.type=emulator,backend.version=2.0,backend.debug=3,backend.source.type=dir,backend.source.path=/some/dir,backend.profile.source=local:mytest,backend.profile.removeDisabled=check
